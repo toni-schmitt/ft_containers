0fe27fb4887d973786ae4073f5db02db57c47262
Toni Schmitt <tschmitt@student.42heilbronn.de>
2022-04-21 17:07:40 +0200

    hups

--- ft_containers/ft_vector.hpp
+++ ft_containers/ft_vector.hpp
@@ -9,18 +9,18 @@ namespace ft
 	class vector
 	{
 		// Member Types
-		typedef value_type T;
-		typedef allocator_type Alloc;
-		typedef reference allocator_type::reference;
-		typedef const_reference allocator_type::const_reference;
-		typedef pointer allocator_type::pointer;
-		typedef const_pointer allocator_type::const_pointer;
-		typedef iterator ft::random_access_iterator<value_type>;
+		typedef T value_type;
+		typedef Alloc allocator_type;
+		typedef allocator_type::reference reference;
+		typedef allocator_type::const_reference const_reference;
+		typedef allocator_type::pointer pointer;
+		typedef allocator_type::const_pointer const_pointer;
+		typedef ft::random_access_iterator<value_type> iterator;
 		// typedef const_iterator ...;
 		// typedef reverse_iterator ...;
 		// typedef const_reverse_iterator ...;
-		typedef difference_type ptrdiff_t;
-		typedef size_type size_t;
+		typedef ptrdiff_t difference_type;
+		typedef size_t size_type;
 
 	};
 } // namespace ft
