a66a8dd19c8e65a3b09546d1a410ff80abb7df36
Toni Schmitt <tschmitt@student.42heilbronn.de>
2022-04-21 14:49:47 +0200

    add basic member types except iterators

--- ft_containers/ft_vector.hpp
+++ ft_containers/ft_vector.hpp
@@ -7,6 +7,19 @@ namespace ft
 	template <class T, class Alloc = std::allocator<T> >
 	class vector
 	{
+		// Member Types
+		typedef value_type T;
+		typedef allocator_type Alloc;
+		typedef reference allocator_type::reference;
+		typedef const_reference allocator_type::const_reference;
+		typedef pointer allocator_type::pointer;
+		typedef const_pointer allocator_type::const_pointer;
+		// typedef iterator ...;
+		// typedef const_iterator ...;
+		// typedef reverse_iterator ...;
+		// typedef const_reverse_iterator ...;
+		typedef difference_type ptrdiff_t;
+		typedef size_type size_t;
 
 	};
 } // namespace ft
