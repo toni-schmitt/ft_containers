87d30f1dd6b4c035c914ce7582a00c289c3fa3be
Toni Schmitt <tschmitt@student.42heilbronn.de>
2022-04-25 21:36:28 +0200

    add basic member function prototypes

--- ft_containers/ft_vector.hpp
+++ ft_containers/ft_vector.hpp
@@ -9,6 +9,7 @@ namespace ft
 	template <class T, class Alloc = std::allocator<T> >
 	class vector
 	{
+#pragma region Member Types
 		/* Member Types */
 		typedef T value_type;
 		typedef Alloc allocator_type;
@@ -22,6 +23,105 @@ namespace ft
 		typedef ft::reverse_iterator<const_iterator> const_reverse_iterator;
 		typedef ptrdiff_t difference_type;
 		typedef size_t size_type;
+#pragma endregion
 
+#pragma region Constructors
+		/* Constructors */
+		explicit vector(const allocator_type &alloc = allocator_type());
+		explicit vector(size_type n, const value_type &val = value_type(), const allocator_type &alloc = allocator_type());
+		template <class InputIterator>
+		vector(InputIterator first, InputIterator last, const allocator_type &alloc = allocator_type());
+		vector(const vector &x);
+#pragma endregion
+
+#pragma region Destructors
+		/* Destructors */
+		~vector();
+#pragma endregion
+
+#pragma region Public Member Functions
+		/* Public Member Functions */
+
+#pragma region Iterators
+		/* Iterator functions */
+
+		iterator begin();
+		const_iterator begin() const;
+
+		iterator end();
+		const_iterator end() const;
+
+		reverse_iterator rbegin();
+		const_reverse_iterator rbegin() const;
+
+		reverse_iterator rend();
+		const_reverse_iterator rend() const;
+
+#pragma endregion
+
+#pragma region Capacity
+		/* Capacity functions */
+
+		size_type size() const;
+		size_type max_size() const;
+		size_type capacity() const;
+
+		bool empty() const;
+
+		void resize(size_type n, value_type val = value_type());
+
+		void reserve(size_type n);
+
+#pragma endregion
+
+#pragma region Element access
+		/* Element access functions */
+
+		reference operator[](size_type n);
+		const_reference operator[](size_type n) const;
+
+		reference at(size_type n);
+		const_reference at(size_type n) const;
+
+		reference front();
+		const_reference front() const;
+
+		reference back();
+		const_reference back() const;
+
+#pragma endregion
+
+#pragma region Modifiers
+		/* Modifiers functions */
+
+		template <class InputIterator>
+		void assign(InputIterator first, InputIterator last);
+		void assign(size_type n, const value_type &val);
+
+		void push_back(const value_type &val);
+		void pop_back();
+
+		iterator insert(iterator position, const value_type &val);
+		void insert(iterator position, size_type n, const value_type &val);
+		template <class InputIterator>
+		void insert(iterator position, InputIterator first, InputIterator last);
+
+		iterator erase(iterator position);
+		iterator erase(iterator first, iterator last);
+
+		void swap(vector &x);
+
+		void clear();
+
+#pragma endregion
+
+#pragma region Allocator
+		/* Allocator functions */
+
+		allocator_type get_allocator() const;
+
+#pragma endregion
+
+#pragma endregion
 	};
 } // namespace ft
