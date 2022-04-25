83f3f77e2afbaed74798ed0bab3eef0a8662dace
Toni Schmitt <tonischmitt@online.de>
2022-04-26 00:13:42 +0200

    uses typename now

--- ft_containers/ft_vector.hpp
+++ ft_containers/ft_vector.hpp
@@ -14,10 +14,10 @@ namespace ft
 	public:
 		typedef T value_type;
 		typedef Alloc allocator_type;
-		typedef allocator_type::reference reference;
-		typedef allocator_type::const_reference const_reference;
-		typedef allocator_type::pointer pointer;
-		typedef allocator_type::const_pointer const_pointer;
+		typedef typename allocator_type::reference reference;
+		typedef typename allocator_type::const_reference const_reference;
+		typedef typename allocator_type::pointer pointer;
+		typedef typename allocator_type::const_pointer const_pointer;
 		typedef ft::random_access_iterator<value_type> iterator;
 		typedef ft::random_access_iterator<const value_type> const_iterator;
 		typedef ft::reverse_iterator<iterator> reverse_iterator;
