106700df767b604aba1e98b2e705fc01039feb3f
Toni Schmitt <tschmitt@student.42heilbronn.de>
2022-04-26 21:03:43 +0200

    add iterator header for all iterator related functions / templates

---
+++ ft_containers/ft_iterator/ft_iterator.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "ft_iterator_traits.hpp"
+#include "ft_forward_iterator.hpp"
+#include "ft_bidirectional_iterator.hpp"
+#include "ft_random_access_iterator.hpp"
+#include "ft_reverse_iterator.hpp"
+#include "ft_iterator_utils.hpp"
