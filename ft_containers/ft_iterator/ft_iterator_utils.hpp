034c5ec39ca63ea83d4ed230e20c87c00d06dc4b
Toni Schmitt <tschmitt@student.42heilbronn.de>
2022-04-26 21:03:28 +0200

    add iterator utils

---
+++ ft_containers/ft_iterator/ft_iterator_utils.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "ft_iterator_traits.hpp"
+
+namespace ft
+{
+
+	/* Distance Function */
+	/* Generic Template */
+	template <class InputIterator>
+	typename iterator_traits<InputIterator>::difference_type
+	distance(InputIterator first, InputIterator last)
+	{
+		return _distance(first, last, typename iterator_traits<InputIterator>::iterator_category);
+	}
+	/* Input Iterator Template */
+	template <class InputIterator>
+	typename iterator_traits<InputIterator>::difference_type
+	_distance(InputIterator first, InputIterator last, input_iterator_tag)
+	{
+		typename iterator_traits<InputIterator>::difference_type dist = 0;
+		while (first != last)
+		{
+			++first;
+			++dist;
+		}
+		return dist;
+	}
+	/* Random Access Itearator Template */
+	template <class InputIterator>
+	typename iterator_traits<InputIterator>::difference_type
+	_distance(InputIterator first, InputIterator last, random_access_iterator_tag)
+	{
+		return last - first;
+	}
+} // namespace ft
