22421eae66a268b6f4607bdb916593bbe279217f
Toni Schmitt <tonischmitt@online.de>
2022-05-01 21:42:12 +0200

    rbegin and rend now return const_reverse_iterators

--- ft_containers/vector.hpp
+++ ft_containers/vector.hpp
@@ -150,10 +150,10 @@ namespace ft
 		const_iterator end() const { return const_iterator(this->_content._end); }
 
 		reverse_iterator rbegin() { return reverse_iterator(end()); }
-		const_reverse_iterator rbegin() const { return reverse_iterator(end()); }
+		const_reverse_iterator rbegin() const { return const_reverse_iterator(end()); }
 
 		reverse_iterator rend() { return reverse_iterator(begin()); }
-		const_reverse_iterator rend() const { return reverse_iterator(begin()); }
+		const_reverse_iterator rend() const { return const_reverse_iterator(begin()); }
 
 		/* Capacity functions */
 	public:
