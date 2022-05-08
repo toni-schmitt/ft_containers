6329756aa0007f7d3c2b27114cf0abc930b128bd
Toni Schmitt <tonischmitt@online.de>
2022-05-08 13:59:49 +0200

    fix: assign operator now only can be called by non integral types

--- ft_containers/vector.hpp
+++ ft_containers/vector.hpp
@@ -232,7 +232,8 @@ namespace ft
 		/* Modifiers functions */
 	public:
 		template <class InputIterator>
-		void assign(InputIterator first, InputIterator last)
+		void assign(InputIterator first, InputIterator last,
+					typename ft::enable_if<!ft::is_integral<InputIterator>::value, bool>::type = true)
 		{
 			const size_type new_size = ft::distance(first, last);
 
