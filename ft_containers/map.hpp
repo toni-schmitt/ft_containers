280976f2c57ec2f42d341cdd4ab557f67c9bd49f
Toni Schmitt <tonischmitt@online.de>
2022-05-09 22:25:08 +0200

    format document

--- ft_containers/map.hpp
+++ ft_containers/map.hpp
@@ -6,11 +6,7 @@
 
 namespace ft
 {
-	template <class Key,											// map::key_type
-			  class T,												// map::mapped_type
-			  class Compare = std::less<Key>,						// map::key_compare
-			  class Alloc = std::allocator<ft::pair<const Key, T> > // map::allocator_type
-			  >
+	template <class Key, class T, class Compare = std::less<Key>, class Alloc = std::allocator<ft::pair<const Key, T> > >
 	class map
 	{
 		/* Member Types */
