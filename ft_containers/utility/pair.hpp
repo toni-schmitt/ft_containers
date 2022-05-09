3f18f394d0c41e0b31b3a1822ef06a486177feea
Toni Schmitt <tonischmitt@online.de>
2022-05-09 22:22:18 +0200

    init: pair

---
+++ ft_containers/utility/pair.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+namespace ft
+{
+	template <class T1, class T2>
+	struct pair
+	{
+		/* Member Types */
+	public:
+		typedef T1 first_type;
+		typedef T2 second_type;
+
+		/* Public Members */
+	public:
+		first_type first;
+		second_type second;
+
+		/* Constructors */
+	public:
+		/* Default Constructor */
+		pair();
+		/* Copy Constructor */
+		template <class U, class V>
+		pair(const pair<U, V> &pr);
+		/* Initialization Constructor */
+		pair(const first_type &a, const second_type &b);
+
+		/* Public Member Functions */
+	public:
+		pair &operator=(const pair &pr);
+	};
+
+	/* Non-member function overloads */
+
+	/* Relational Operators */
+	template <class T1, class T2>
+	bool operator==(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs);
+	template <class T1, class T2>
+	bool operator!=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs);
+	template <class T1, class T2>
+	bool operator<(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs);
+	template <class T1, class T2>
+	bool operator<=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs);
+	template <class T1, class T2>
+	bool operator>(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs);
+	template <class T1, class T2>
+	bool operator>=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs);
+
+} // namespace ft
