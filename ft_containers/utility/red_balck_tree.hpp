789e718b7f9ba8a95a7d13796ce160c86cd5bcc9
Toni Schmitt <tschmitt@student.42heilbronn.de>
2022-06-07 18:08:55 +0200

    add: basic rbt_tree and node structure

---
+++ ft_containers/utility/red_balck_tree.hpp
@@ -0,0 +1,134 @@
+#pragma once
+
+#include <memory>
+
+#define rbt_node_left_child 0;
+#define rbt_node_right_child 1;
+
+namespace ft
+{
+
+	/**
+	 * @brief Node Structor for Red Black Tree
+	 *
+	 * @tparam T Value Type of Node Data
+	 * @tparam Alloc Allocator of Value Type (Defaults to std::allocator<T>)
+	 */
+	template <class T, class Alloc = std::allocator<T> >
+	struct rbt_node
+	{
+		/* Member Types */
+	public:
+		typedef T value_type;
+		typedef Alloc allocator_type;
+		typedef typename allocator_type::reference value_ref;
+		typedef typename allocator_type::const_reference value_const_ref;
+		typedef typename allocator_type::pointer value_ptr;
+		typedef typename allocator_type::const_pointer value_const_ptr;
+		typedef rbt_node &node_ref;
+		typedef const rbt_node &const_node_ref;
+		typedef rbt_node *node_ptr;
+		typedef const rbt_node *const_node_ptr;
+
+	public:
+		enum node_color
+		{
+			red = 0,
+			black = 1
+		};
+
+		/* Protected Members */
+	protected:
+		value_type data;
+		node_ptr children[2];
+		node_color color;
+
+		/* Private Functions */
+	private:
+		/* Constructors */
+	public:
+		/**
+		 * @brief Construct a new rbt node object
+		 *
+		 * @param data Data to store in Node
+		 * @param color Color of Node (see enum node_color)
+		 * @param left_child Left Child of Node
+		 * @param right_child Right Child of Node
+		 */
+		rbt_node(value_const_ref data, node_color color, node_ptr left_child = NULL, node_ptr right_child = NULL)
+		{
+			this->data = data;
+			this->children[rbt_node_left_child] = left_child;
+			this->children[rbt_node_right_child] = right_child;
+			this->color = color;
+		}
+
+		/**
+		 * @brief Construct a new rbt node object from copy
+		 *
+		 * @param copy Copy of which to construct object
+		 */
+		rbt_node(const_node_ref copy)
+		{
+			if (&copy == this)
+				return *this;
+
+			this->data = copy.data;
+			this->color = copy.color;
+			this->children = copy.children;
+
+			return *this;
+		}
+
+		/* Destructors */
+	public:
+		~rbt_node() {}
+
+		/* Modifiers functions */
+	public:
+	};
+
+	// search, insert, delete (main operations)
+	// helpers: rotation, color flip
+	template <class T, class Alloc = std::allocator<T> >
+	class red_black_tree
+	{
+		/* Member Types */
+	public:
+		typedef T value_type;
+		typedef Alloc allocator_type;
+		typedef typename allocator_type::reference reference;
+		typedef typename allocator_type::const_reference const_reference;
+		typedef typename allocator_type::pointer pointer;
+		typedef typename allocator_type::const_pointer const_pointer;
+
+	private:
+		typedef rbt_node<value_type, allocator_type> node_type;
+		typedef typename node_type::node_ref node_ref;
+		typedef typename node_type::const_node_ref const_node_ref;
+		typedef typename node_type::node_ptr node_ptr;
+		typedef typename node_type::const_node_ptr const_node_ptr;
+		typedef typename node_type::node_color node_color;
+
+		/* Private Members */
+	private:
+		node_ptr _root;
+
+		/* Private Functions */
+	private:
+		/* Constructors */
+	public:
+		/* Destructors */
+	public:
+		/* Public Member Functions */
+	public:
+		/* Iterator functions */
+	public:
+		/* Capacity functions */
+	public:
+		/* Element access functions */
+	public:
+		/* Modifiers functions */
+	public:
+	};
+} // namespace ft
