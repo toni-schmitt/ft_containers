c6714b5a8b4ea8dbfad67dd24228c50110e4c02b
toni-schmitt <tonischmitt@online.de>
2022-09-19 17:34:21 +0200

    ADD (iterator_utils): get_raw_pointer function for getting raw pointer form iterator

--- ft_containers/iterator/iterator_utils.hpp
+++ ft_containers/iterator/iterator_utils.hpp
@@ -13,20 +13,14 @@ namespace ft
 	typename std::iterator_traits<InputIterator>::difference_type
 	_distance(InputIterator first, InputIterator last, std::input_iterator_tag)
 	{
-		typename std::iterator_traits<InputIterator>::difference_type dist = 0;
-		while (first != last)
-		{
-			++first;
-			++dist;
-		}
-		return dist;
+		return std::distance(first, last);
 	}
 	/* Random Access Iterator Template */
 	template <class InputIterator>
 	typename std::iterator_traits<InputIterator>::difference_type
 	_distance(InputIterator first, InputIterator last, std::random_access_iterator_tag)
 	{
-		return last - first;
+		return std::distance(first, last);
 	}
 	/* For FT Iterators: */
 	/* Input Iterator Template */
@@ -56,4 +50,126 @@ namespace ft
 	{
 		return _distance(first, last, typename iterator_traits<InputIterator>::iterator_category());
 	}
+
+	template <class Iterator>
+	typename iterator_traits<Iterator>::pointer get_raw_pointer(Iterator iter)
+	{
+		return iter.base();
+	}
+
+	namespace
+	{
+		/* STD ITERATORS */
+
+		template <class InputIterator>
+		void _advance(InputIterator &i, typename iterator_traits<InputIterator>::difference_type n, std::input_iterator_tag)
+		{
+			return std::advance(i, n);
+		}
+		template <class BidirectionalIterator>
+		void _advance(BidirectionalIterator &i, typename iterator_traits<BidirectionalIterator>::difference_type n, std::bidirectional_iterator_tag)
+		{
+			return std::advance(i, n);
+		}
+		template <class RandomAccessIterator>
+		void _advance(RandomAccessIterator &i, typename iterator_traits<RandomAccessIterator>::difference_type n, std::random_access_iterator_tag)
+		{
+			return std::advance(i, n);
+		}
+
+		/* FT ITERATORS */
+
+		template <class InputIterator>
+		void _advance(InputIterator &i, typename iterator_traits<InputIterator>::difference_type n, ft::input_iterator_tag)
+		{
+			while (n > 0)
+			{
+				++i;
+				--n;
+			}
+		}
+		template <class BidirectionalIterator>
+		void _advance(BidirectionalIterator &i, typename iterator_traits<BidirectionalIterator>::difference_type n, ft::bidirectional_iterator_tag)
+		{
+			if (n >= 0)
+			{
+				while (n > 0)
+				{
+					++i;
+					--n;
+				}
+			}
+			else
+			{
+				while (n < 0)
+				{
+					--i;
+					++n;
+				}
+			}
+		}
+		template <class RandomAccessIterator>
+		void _advance(RandomAccessIterator &i, typename iterator_traits<RandomAccessIterator>::difference_type n, ft::random_access_iterator_tag)
+		{
+			i += n;
+		}
+	} // private namespace
+
+	template <class InputIterator>
+	void advance(InputIterator &i, typename iterator_traits<InputIterator>::difference_type n)
+	{
+		_advance(i, n, typename iterator_traits<InputIterator>::iterator_category());
+	}
+
+	namespace
+	{
+
+		/* STD ITERATORS */
+
+		template <class InputIterator>
+		InputIterator _next(InputIterator it, typename iterator_traits<InputIterator>::difference_type n, std::input_iterator_tag)
+		{
+			return std::next(it, n);
+		}
+		template <class BidirectionalIterator>
+		BidirectionalIterator _next(BidirectionalIterator it, typename iterator_traits<BidirectionalIterator>::difference_type n, std::bidirectional_iterator_tag)
+		{
+			return std::next(it, n);
+		}
+		template <class RandomAccessIterator>
+		RandomAccessIterator _next(RandomAccessIterator it, typename iterator_traits<RandomAccessIterator>::difference_type n, std::random_access_iterator_tag)
+		{
+			return std::next(it, n);
+		}
+
+		/* FT ITERATORS */
+
+		template <class InputIterator>
+		InputIterator _next(InputIterator it, typename iterator_traits<InputIterator>::difference_type n, ft::input_iterator_tag)
+		{
+			ft::advance(it, n);
+			return it;
+		}
+		template <class BidirectionalIterator>
+		BidirectionalIterator _next(BidirectionalIterator it, typename iterator_traits<BidirectionalIterator>::difference_type n, ft::bidirectional_iterator_tag)
+		{
+			ft::advance(it, n);
+			return it;
+		}
+		template <class RandomAccessIterator>
+		RandomAccessIterator _next(RandomAccessIterator it, typename iterator_traits<RandomAccessIterator>::difference_type n, ft::random_access_iterator_tag)
+		{
+			ft::advance(it, n);
+			return it;
+		}
+
+	} // private namespace
+
+	template <class Iterator>
+	Iterator next(Iterator it,
+						 typename iterator_traits<Iterator>::difference_type n = 1)
+	{
+		return _next(it, n, typename iterator_traits<Iterator>::iterator_category());
+	}
+
 } // namespace ft
