e99187f1ed7539a3fcdfb59c475e6cdd091415bc
toni-schmitt <tonischmitt@online.de>
2022-10-20 16:03:59 +0200

    ADD (pair): Implementation
    Signed-off-by: toni-schmitt <tonischmitt@online.de>

--- ft_containers/utility/pair.hpp
+++ ft_containers/utility/pair.hpp
@@ -18,43 +18,61 @@ namespace ft
 		/* Constructors */
 	public:
 		/* Default Constructor */
-		pair();
+		pair() : first(), second() { }
 
 		/* Copy Constructor */
 		template < class U, class V >
-		pair(const pair<U, V> &pr);
+		pair(const pair<U, V> &pr) : first(pr.first), second(pr.second) { }
 
 		/* Initialization Constructor */
-		pair(const first_type &a, const second_type &b);
+		pair(const first_type &a, const second_type &b) : first(a), second(b) { }
 
 		/* Public Member Functions */
 	public:
-		pair &operator=(const pair &pr);
+		pair &operator=(const pair &pr)
+		{
+			if (this == &pr)
+				return *this;
+
+			this->first = pr.first;
+			this->second = pr.second;
+
+			return *this;
+		}
 	};
 
 	/* Non-member function overloads */
 
 	/* Relational Operators */
 	template < class T1, class T2 >
-	bool operator==(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs);
+	bool operator==(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs)
+	{
+		return lhs.first == rhs.first && lhs.second == rhs.second;
+	}
 
 	template < class T1, class T2 >
-	bool operator!=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs);
+	bool operator!=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) { return !(lhs == rhs); }
 
 	template < class T1, class T2 >
-	bool operator<(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs);
+	bool operator<(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs)
+	{
+		return (lhs.first < rhs.first || (!(rhs.first < lhs.first) && lhs.second < rhs.second));
+	}
 
 	template < class T1, class T2 >
-	bool operator<=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs);
+	bool operator<=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs)
+	{
+		return (lhs.first <= rhs.first || (!(rhs.first <= lhs.first) && lhs.second <= rhs.second));
+	}
 
 	template < class T1, class T2 >
-	bool operator>(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs);
+	bool operator>(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) { return (rhs < lhs); }
 
 	template < class T1, class T2 >
-	bool operator>=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs);
+	bool operator>=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) { return (!(lhs <= rhs)); }
 
 
 	template < class T1, class T2 >
-	ft::pair<T1, T2> make_pair(T1 x, T2 y);
+	ft::pair<T1, T2> make_pair(T1 x, T2 y) { return ft::pair<T1, T2>(x, y); }
 
 } // namespace ft
