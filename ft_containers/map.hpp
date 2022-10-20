9377efcf1abe8fe1ee38a4e751c89642b7766d1a
toni-schmitt <tonischmitt@online.de>
2022-10-20 16:35:04 +0200

    ADD (map): Relational Operators Overload
    Signed-off-by: toni-schmitt <tonischmitt@online.de>

--- ft_containers/map.hpp
+++ ft_containers/map.hpp
@@ -323,6 +323,42 @@ namespace ft
 		allocator_type get_allocator() const { return this->_alloc; }
 	};
 
+	/* Relational Operators Overload */
+	template < class Key, class T, class Compare, class Alloc >
+	bool operator==(const map<Key, T, Compare, Alloc> &lhs, const map<Key, T, Compare, Alloc> &rhs)
+	{
+		return lhs.size() == rhs.size() && ft::equal(lhs.begin(), lhs.end(), rhs.begin());
+	}
+
+	template < class Key, class T, class Compare, class Alloc >
+	bool operator!=(const map<Key, T, Compare, Alloc> &lhs, const map<Key, T, Compare, Alloc> &rhs)
+	{
+		return !(lhs == rhs);
+	}
+
+	template < class Key, class T, class Compare, class Alloc >
+	bool operator<(const map<Key, T, Compare, Alloc> &lhs, const map<Key, T, Compare, Alloc> &rhs)
+	{
+		return ft::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+	}
+
+	template < class Key, class T, class Compare, class Alloc >
+	bool operator<=(const map<Key, T, Compare, Alloc> &lhs, const map<Key, T, Compare, Alloc> &rhs)
+	{
+		return !(rhs < lhs);
+	}
+
+	template < class Key, class T, class Compare, class Alloc >
+	bool operator>(const map<Key, T, Compare, Alloc> &lhs, const map<Key, T, Compare, Alloc> &rhs) { return rhs < lhs; }
+
+	template < class Key, class T, class Compare, class Alloc >
+	bool operator>=(const map<Key, T, Compare, Alloc> &lhs, const map<Key, T, Compare, Alloc> &rhs)
+	{
+		return !(lhs < rhs);
+	}
+
+
+	/* Swap */
 	template < class Key, class T, class Compare, class Alloc >
 	void swap(map<Key, T, Compare, Alloc> &x, map<Key, T, Compare, Alloc> &y) { x.swap(y); }
 } // namespace ft
